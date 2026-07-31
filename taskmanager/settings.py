7e84a5172819f3ae9fd079135979e4688936573a
ousmane <sudouzi@mbp-de-ousmane.home>
2026-07-31 17:54:45 +0000

    Fix Railway static files and CSRF

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -37,6 +37,15 @@ ALLOWED_HOSTS = [
     "*"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://todolistouzi.up.railway.app",
+]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
